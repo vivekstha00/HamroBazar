ae6e94d30403cc1334f0e6f4aa172fe29ced7700
Vivek Shrestha <vivekstha00@gmail.com>
2025-12-25 02:55:36 +0545

    Fix tests by bootstrapping app and running migrations

--- app/Http/Controllers/Frontend/BaseController.php
+++ app/Http/Controllers/Frontend/BaseController.php
@@ -4,17 +4,27 @@ namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Color;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 
 class BaseController extends Controller
 {
     public function __construct()
     {
-        $color = Color::first();
+        $defaultColor = (object)[
+            'primary' => '#0d6efd',
+            'secondary' => '#6c757d',
+            'text' => '#212529',
+            'bg' => '#ffffff',
+        ];
 
-        View::share([
-            "color" => $color,
-        ]);
+        if (!Schema::hasTable('colors')) {
+            View::share(['color' => $defaultColor]);
+            return;
+        }
+
+        $color = Color::first() ?? $defaultColor;
+
+        View::share(['color' => $color]);
     }
 }
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,13 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Run migrations for the in-memory SQLite database used by phpunit.xml
+        $this->artisan('migrate');
+    }
 }
